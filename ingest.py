f56159c990519cfa5d871cac500258a5d27d2fa4
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2025-04-06 22:03:14 +0000

    uv script

--- ingest.py
+++ ingest.py
@@ -1,7 +1,7 @@
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
-#     "duckdb",
+#     "duckdb==1.1.0",
 #     "requests",
 # ]
 # ///
@@ -11,6 +11,7 @@ import requests
 import duckdb
 from urllib.request import urlretrieve
 from datetime import datetime, timedelta
+import tempfile
 
 # We take file logs-all.parquet
 # If exists:
@@ -29,82 +30,84 @@ def main():
 
     url = azure_url("logs.parquet", args.blob_sas_url)
     response = requests.head(url)
-    if response.status_code != 404:
-        urlretrieve(url, "old-logs.parquet")
-        duckdb.sql(
-            """
-            COPY
-            (
-              SELECT
-                remote_addr,
-                remote_user,
-                time,
-                status,
-                body_bytes_sent,
-                http_referer,
-                http_user_agent,
-                http_x_forwarded_for,
-                regexp_extract(request, '(.+) (.+) (.+)', 1) AS method,
-                regexp_extract(request, '(.+) (.+) (.+)', 2) AS path,
-                regexp_extract(request, '(.+) (.+) (.+)', 3) AS protocol,
-              FROM read_csv('%s', columns = {
-                'remote_addr': 'VARCHAR',
-                'remote_user': 'VARCHAR',
-                'time': 'TIMESTAMPTZ',
-                'request': 'VARCHAR',
-                'status': 'SMALLINT',
-                'body_bytes_sent': 'INTEGER',
-                'http_referer': 'VARCHAR',
-                'http_user_agent': 'VARCHAR',
-                'http_x_forwarded_for': 'VARCHAR',
-              })
-              WHERE
-                time > ( SELECT MAX(time) FROM read_parquet('old-logs.parquet'))
-              UNION
-              SELECT * FROM read_parquet('old-logs.parquet')
-            )
-            TO 'logs.parquet'
-            (FORMAT 'parquet', COMPRESSION 'zstd')
-            """ % args.log_file )
-        requests.delete(url)
-    else:
-        duckdb.sql(
-            """
-            COPY
-            (
-              SELECT
-                remote_addr,
-                remote_user,
-                time,
-                status,
-                body_bytes_sent,
-                http_referer,
-                http_user_agent,
-                http_x_forwarded_for,
-                regexp_extract(request, '(.+) (.+) (.+)', 1) AS method,
-                regexp_extract(request, '(.+) (.+) (.+)', 2) AS path,
-                regexp_extract(request, '(.+) (.+) (.+)', 3) AS protocol,
-              FROM read_csv('%s', columns = {
-                'remote_addr': 'VARCHAR',
-                'remote_user': 'VARCHAR',
-                'time': 'TIMESTAMPTZ',
-                'request': 'VARCHAR',
-                'status': 'SMALLINT',
-                'body_bytes_sent': 'INTEGER',
-                'http_referer': 'VARCHAR',
-                'http_user_agent': 'VARCHAR',
-                'http_x_forwarded_for': 'VARCHAR',
-              })
-            )
-            TO 'logs.parquet'
-            (FORMAT 'parquet', COMPRESSION 'zstd')
-            """ % args.log_file )
+    with tempfile.NamedTemporaryFile() as logs_fp:
+        if response.status_code != 404:
+            with tempfile.NamedTemporaryFile() as old_logs_fp:
+                urlretrieve(url, old_logs_fp.name)
+                duckdb.sql(
+                """
+                COPY
+                (
+                  SELECT
+                    remote_addr,
+                    remote_user,
+                    time,
+                    status,
+                    body_bytes_sent,
+                    http_referer,
+                    http_user_agent,
+                    http_x_forwarded_for,
+                    regexp_extract(request, '(.+) (.+) (.+)', 1) AS method,
+                    regexp_extract(request, '(.+) (.+) (.+)', 2) AS path,
+                    regexp_extract(request, '(.+) (.+) (.+)', 3) AS protocol,
+                  FROM read_csv('%s', columns = {
+                    'remote_addr': 'VARCHAR',
+                    'remote_user': 'VARCHAR',
+                    'time': 'TIMESTAMPTZ',
+                    'request': 'VARCHAR',
+                    'status': 'SMALLINT',
+                    'body_bytes_sent': 'INTEGER',
+                    'http_referer': 'VARCHAR',
+                    'http_user_agent': 'VARCHAR',
+                    'http_x_forwarded_for': 'VARCHAR',
+                  })
+                  WHERE
+                    time > ( SELECT MAX(time) FROM read_parquet('%s'))
+                  UNION
+                  SELECT * FROM read_parquet('%s')
+                )
+                TO '%s'
+                (FORMAT 'parquet', COMPRESSION 'zstd')
+                """ % (args.log_file, old_logs_fp.name, old_logs_fp.name, logs_fp.name) )
+                requests.delete(url)
+        else:
+            duckdb.sql(
+                """
+                COPY
+                (
+                  SELECT
+                    remote_addr,
+                    remote_user,
+                    time,
+                    status,
+                    body_bytes_sent,
+                    http_referer,
+                    http_user_agent,
+                    http_x_forwarded_for,
+                    regexp_extract(request, '(.+) (.+) (.+)', 1) AS method,
+                    regexp_extract(request, '(.+) (.+) (.+)', 2) AS path,
+                    regexp_extract(request, '(.+) (.+) (.+)', 3) AS protocol,
+                  FROM read_csv('%s', columns = {
+                    'remote_addr': 'VARCHAR',
+                    'remote_user': 'VARCHAR',
+                    'time': 'TIMESTAMPTZ',
+                    'request': 'VARCHAR',
+                    'status': 'SMALLINT',
+                    'body_bytes_sent': 'INTEGER',
+                    'http_referer': 'VARCHAR',
+                    'http_user_agent': 'VARCHAR',
+                    'http_x_forwarded_for': 'VARCHAR',
+                  })
+                )
+                TO '%s'
+                (FORMAT 'parquet', COMPRESSION 'zstd')
+                """ % (args.log_file, logs_fp.name) )
 
-    requests.put(url, headers={
-        "Content-Type": "application/vnd.apache.parquet",
-        "x-ms-blob-type": "BlockBlob"
-    }, data=open("logs.parquet", "rb"))
-        
+        requests.put(url, headers={
+            "Content-Type": "application/vnd.apache.parquet",
+            "x-ms-blob-type": "BlockBlob"
+        }, data=open(logs_fp.name, "rb"))
+            
 
 def azure_url(filename, blob_sas_url):
     blob_split_url = blob_sas_url.split("?")
